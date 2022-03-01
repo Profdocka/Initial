db00039ed6c025d96a7621809da8127962b33451
Profdocka <tobiasluca@ewe.net>
2022-03-01 21:09:20 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   url = "https://api.igdb.com/v4/games";
-  header = new HttpHeaders({"Content-Type": "text/plain" , "Client-ID": "56pmsmf23lb6a8rn0z6t8vvg47r0a2" , "Authorization" : "Bearer ud22j0xr53lb06mep6au7m8mxcogx3"})
-
+  header = new HttpHeaders({"Client-ID": "56pmsmf23lb6a8rn0z6t8vvg47r0a2" , 'Authorization' : "Bearer ud22j0xr53lb06mep6au7m8mxcogx3"})
   constructor(private http: HttpClient) {
-    http.post(this.url, {headers: this.header}).subscribe();
+  }
+
+  ngOnInit() {
+    this.http.post(this.url, {headers: this.header}).subscribe(data => console.log(data));
   }
 }
 
