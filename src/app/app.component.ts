0902181169008bb67b7b508bd5f7219e9e5fd369
Profdocka <tobiasluca@ewe.net>
2022-03-01 17:07:01 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
@@ -7,6 +8,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  constructor(private http: HttpClient) {
+    let url = "https://api.igdb.com/v4/games";
+    let header = new HttpHeaders({"Client-ID": "56pmsmf23lb6a8rn0z6t8vvg47r0a2" , Authorization : "Bearer ud22j0xr53lb06mep6au7m8mxcogx3"})
+    http.post(url, {headers: header}).subscribe( => { data.log(data)});
+  }
 }
 
 //d 
