df5bce5a875f80c3e6e1926db956b40a942663b5
Profdocka <tobiasluca@ewe.net>
2022-03-03 10:27:13 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
@@ -8,14 +8,21 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-  url = "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games/v4/games";
-  header = new HttpHeaders({"Client-ID": "56pmsmf23lb6a8rn0z6t8vvg47r0a2" , 'Authorization' : "Bearer ud22j0xr53lb06mep6au7m8mxcogx3"})
+  url = "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games/";
+  //header = new HttpHeaders({'Client-ID': '56pmsmf23lb6a8rn0z6t8vvg47r0a2' , 'Authorization': "Bearer ud22j0xr53lb06mep6au7m8mxcogx3"});
+  header = new HttpHeaders()
+  .set('Client-ID','56pmsmf23lb6a8rn0z6t8vvg47r0a2')
+  .set('Authorization', "Bearer ud22j0xr53lb06mep6au7m8mxcogx3")
+  params = new HttpParams()
+
   constructor(private http: HttpClient) {
+    this.http.get(this.url, {headers :this.header, params: this.params}).subscribe(data => console.log(data));
   }
 
   ngOnInit() {
-    this.http.post(this.url, {headers: this.header}).subscribe(data => console.log(data));
+    
   }
 }
-
+//Hier hat Kaiwen 17 Stifte, eine Karotte und ein USB Kabel ins Anus gesteckt
 //d 
+// 
